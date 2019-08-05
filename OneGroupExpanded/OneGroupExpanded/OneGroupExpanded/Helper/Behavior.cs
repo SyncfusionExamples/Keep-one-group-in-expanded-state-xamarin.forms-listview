15593c86029987481a17cc0db77d2544bccf8ba2
Gnana Priya N <gnanapriya.namasivayam@syncfusion.com>
2019-08-05 07:39:23 +0530

    Updated sample and read me file

---
+++ OneGroupExpanded/OneGroupExpanded/OneGroupExpanded/Helper/Behavior.cs
@@ -0,0 +1,70 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.DataSource.Extensions;
+using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewSample
+{
+   public class Behavior : Behavior<SfListView>
+    {
+
+        #region Fields
+
+        GroupResult expandedGroup;
+        SfListView listView;
+        #endregion
+
+        protected override void OnAttachedTo(SfListView bindable)
+        {
+            listView = bindable;
+            listView.DataSource.SortDescriptors.Add(new SortDescriptor() { PropertyName = "ContactName", Direction = ListSortDirection.Ascending });
+            listView.DataSource.GroupDescriptors.Add(new GroupDescriptor()
+            {
+                PropertyName = "ContactName",
+                KeySelector = (object obj1) =>
+                {
+                    var item = (obj1 as Contacts);
+                    return item.ContactName[0].ToString();
+                },
+            });
+            listView.Loaded += ListView_Loaded;
+            listView.GroupExpanding += ListView_GroupExpanding;
+
+            base.OnAttachedTo(bindable);
+        }
+        protected override void OnDetachingFrom(SfListView bindable)
+        {
+            listView.Loaded -= ListView_Loaded;
+            listView.GroupExpanding -= ListView_GroupExpanding;
+            base.OnDetachingFrom(bindable);
+        }
+        private void ListView_Loaded(object sender, ListViewLoadedEventArgs e)
+        {
+            listView.CollapseAll();
+        }
+
+        private void ListView_GroupExpanding(object sender, GroupExpandCollapseChangingEventArgs e)
+        {
+            if (e.Groups.Count > 0)
+            {
+                var group = e.Groups[0];
+                if (expandedGroup == null || group.Key != expandedGroup.Key)
+                {
+                    foreach (var otherGroup in listView.DataSource.Groups)
+                    {
+                        if (group.Key != otherGroup.Key)
+                        {
+                            listView.CollapseGroup(otherGroup);
+                        }
+                    }
+                    expandedGroup = group;
+                    listView.ExpandGroup(expandedGroup);
+                }
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Keep-one-group-in-expanded-state-xamarin.forms-listview
-Keep one group in expanded state in Xamarin Forms ListView 
+# Keeping only one group in expanded state
+
+To keep any one specific group alone in the expanded state, use the [SfListView.GroupExpanding](https://help.syncfusion.com/cr/cref_files/xamarin/Syncfusion.SfListView.XForms~Syncfusion.ListView.XForms.SfListView~GroupExpanding_EV.html) event. The particular selected group can be get from [GroupExpandCollapseChangingEventArgs](https://help.syncfusion.com/cr/cref_files/xamarin/Syncfusion.SfListView.XForms~Syncfusion.ListView.XForms.GroupExpandCollapseChangingEventHandler.html), by which you can compare and collapse all other groups, and expand the particular selected group.
+
+```
+private void ListView_GroupExpanding(object sender, GroupExpandCollapseChangingEventArgs e) 
+{ 
+  if (e.Groups.Count > 0) 
+  { 
+    var group = e.Groups[0]; 
+    if (expandedGroup == null || group.Key != expandedGroup.Key) 
+    { 
+      foreach (var otherGroup in listView.DataSource.Groups) 
+      { 
+        if (group.Key != otherGroup.Key) 
+        { 
+          listView.CollapseGroup(otherGroup); 
+        } 
+      } 
+      expandedGroup = group; 
+      listView.ExpandGroup(expandedGroup); 
+    } 
+ } 
+}
+```
+
+To know more about grouping, please refer our documentation [here](https://help.syncfusion.com/xamarin/sflistview/grouping)
